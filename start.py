f0482101a6da3c7b6f78eb34f42acfbafa85ec15
Fabio Lopes <fabioacl96@gmail.com>
2025-05-02 14:18:33 +0200

    Correct setup.cfg

--- start.py
+++ start.py
@@ -5,8 +5,6 @@ def get_start_widget(appbase, jupbase, notebase):
     return ipw.HTML(
         f"""
     <div align="center">
-        <a href="{appbase}/interface.ipynb" target="_blank">
-            <img src="https://github.com/nanotech-empa/aiidalab-records-app/blob/main/logo.jpg?raw=true" height="120px" width=243px">
-        </a>
+        <a href="{appbase}/interface.ipynb" target="_blank">Register data records</a>
     </div>"""
     )
